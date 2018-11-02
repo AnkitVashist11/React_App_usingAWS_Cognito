7b2766bb78fd8f552e1a4c391b50b4363d13254b
Ankit Vashist <ankitvashist11@gmail.com>
2018-11-02 16:32:14 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-export default {
-  cognito: {
-    USER_POOL_ID: "us-east-1_6u9dzcNov", //Your created User pool id
-    APP_CLIENT_ID: "50hqmbuefsgb7olormpp7f2jl1" // your created App client id , after making a account and going on aws console
-                                               //and search for cognito service
-  }
-};
+// export default {
+//   cognito: {
+//     USER_POOL_ID: "us-east-1_6u9dzcNov", //Your created User pool id
+//     APP_CLIENT_ID: "50hqmbuefsgb7olormpp7f2jl1" // your created App client id , after making a account and going on aws console
+//                                                //and search for cognito service
+//   }
+// };
